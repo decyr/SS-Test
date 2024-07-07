578c2f357874d3d975c60b340c295f51ef71f27a
decyr <100169209+decyr@users.noreply.github.com>
2024-07-07 17:56:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.getElementById('robloxButton').addEventListener('click', function() {
     fetch('https://your-github-actions-url.com/action', {
-        method: 'POST', // or 'GET' depending on your setup
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            action: 'some_action', // Define the action you want to trigger in your game
+            key: '6230927530_5728571899',
+            script: 'print("Hello world")'
         })
     })
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
-        // Optionally handle success response from the server
     })
     .catch((error) => {
         console.error('Error:', error);
